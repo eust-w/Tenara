16a6e0f47f82bb123ad54b0ce7681f544a832e38
eust-w <eustanceuwu@gmail.com>
2026-08-23 23:30:43 +0800

    feat(build): build crd and controller lifecycle
    plan: tenara-agent-paas#28
    rb: RB-12 RB-34

---
+++ controllers/cmd/build-controller/main.go
@@ -0,0 +1,32 @@
+// Build controller binary for the Tenara platform.
+package main
+
+import (
+	"os"
+
+	"tenara/controllers/internal/build"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+func main() {
+	logger := zap.New(zap.UseDevMode(true))
+	ctrl.SetLogger(logger)
+	mgr, mgrErr := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme: build.Scheme,
+	})
+	if mgrErr != nil {
+		logger.Error(mgrErr, "unable to start manager")
+		os.Exit(1)
+	}
+	if err := build.NewReconciler(mgr).SetupWithManager(mgr); err != nil {
+		logger.Error(err, "unable to create controller")
+		os.Exit(1)
+	}
+	logger.Info("starting build controller")
+	if runErr := mgr.Start(ctrl.SetupSignalHandler()); runErr != nil {
+		logger.Error(runErr, "controller exited")
+		os.Exit(1)
+	}
+}
